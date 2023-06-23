dfd0f715903759a7e283e9e161ce507621f3bd09
ariweather <weatherspoonariel@gmail.com>
2023-06-23 09:43:15 -0500

    Made adjustments to the splash screen and set up launcher icon.

--- app/src/main/java/com/askamareh/splash.java
+++ app/src/main/java/com/askamareh/splash.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 public class splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(() -> {
